28191c3258f10e7104afcc373f7fa6775ee1b2f5
PheonixDeathNote <153576791+PheonixDeathNote@users.noreply.github.com>
2023-12-13 11:54:53 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+
+
+
+## Deploy To Heroku
+
+<a href="https://heroku.com/deploy?template=https://github.com/PheonixDeathNote/musicbot2">
+     <img height="30px" src="https://img.shields.io/badge/Deploy%20To%20Heroku-blueviolet?style=for-the-badge&logo=heroku">
+  </a>
+
---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+API_ID = API_ID = 20463500
+
+API_HASH = os.environ.get("API_HASH", "5db9bc6439f7b43daa75689edf65b431")
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6920803794:AAHlsryRx1vNQe7FgdlbbnhxO6o5aFKr430")
+
+PASS_DB = int(os.environ.get("PASS_DB", "721"))
+
+OWNER = int(os.environ.get("OWNER", 6333211826))
+
+LOG = -1002008774612
+
